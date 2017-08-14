c6a1a7912cd6ca8a2d1a60ae6704e0a2d9b796fc
Thinh Tran <duythinht@gmail.com>
2017-08-14 13:34:05 +0700

    Add testcase for writecsv

--- unique_finder/finder_test.go
+++ unique_finder/finder_test.go
@@ -1,6 +1,12 @@
 package unique_finder
 
-import "testing"
+import (
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"strings"
+	"testing"
+)
 
 func TestPhoneMapAddMember(t *testing.T) {
 	finder := NewFinder()
@@ -20,6 +26,24 @@ func TestListPhoneWithActivate(t *testing.T) {
 	finder.Add("0980000001", "2016-11-01", "2016-12-01")
 	listPhone := finder.ListPhoneWithActivatedTime()
 	if listPhone[0] != "0980000001,2016-09-01" {
+		t.Fail()
+	}
+}
+
+func TestWriteCsv(t *testing.T) {
+	var buffer bytes.Buffer
+	writer := bufio.NewWriter(&buffer)
+	w := csv.NewWriter(writer)
+
+	finder := NewFinder()
+	finder.Add("0980000001", "2016-09-01", "2016-11-01")
+	finder.Add("0980000001", "2016-11-01", "2016-12-01")
+	finder.WriteToCSV(w)
+
+	expected := `0980000001,2016-09-01` //write with end of line
+
+	if !strings.Contains(buffer.String(), expected) {
+		t.Log("output should contains", expected)
 		t.Fail()
 	}
 }
